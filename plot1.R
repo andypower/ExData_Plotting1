570093e691f76a1a872076c3bdc1f875e86e2aca
andypower <ssileno@tiscali.it>
2015-01-12 00:58:32 +0100

    plot1 commit

---
+++ plot1.R
@@ -0,0 +1,36 @@
+# Coursera Exploratory Analysis: First script plot1
+
+# Reading data using the read.csv(..) function and reading only the usefull dates
+# passing ; separator
+# indicating column type
+data <- read.csv("D:/Google Drive/Dottorato/Anno I/Corsi/Exploratory Data Analysis/Week 1/ExData_Plotting1/household_power_consumption.txt", 
+                 skip = 66636, 
+                 nrows = 2880,
+                 sep=";")
+
+names(data) <- c("Date", "Time", "Global_active_power", 
+                 "Global_reactive_power", "Voltage", 
+                 "Global_intensity", "Sub_metering_1", 
+                 "Sub_metering_2", "Sub_metering_3")
+
+# Combining date colums
+data$DateTime <- paste(data$Date, data$Time)
+
+# Setting date format
+data$DateTime <- strptime(data$DateTime, 
+                          "%d/%m/%Y %H:%M:%S")
+
+# Opening PNG file device and setting dimensions
+png(file = "plot1.png", height=480, width=480)
+
+# Set global graphical parameters
+par(mfrow = c(1,1))
+
+# Using histogram function
+hist(data$Global_active_power, 
+     xlab = "Global Active Power (kilowatts)", 
+     main = "Global Active Power",
+     col = "red")
+
+# Closing PNG file device
+dev.off()
